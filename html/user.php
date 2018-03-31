3fd911b2e0cd22cb149ecf3039bad23ba1ef94b5
Helen Õunapuu <helen.ounapuu@gmail.com>
2018-03-31 16:41:05 +0300

    file upload fix4

--- html/user.php
+++ html/user.php
@@ -30,51 +30,10 @@ if (!isset($_SESSION['fb_access_token'])) {
 
 <p></br><br/><br/></p>
 
-<?php
-if(isset($_FILES['image'])){
-    $errors= array();
-    $file_name = $_FILES['image']['name'];
-    $file_size =$_FILES['image']['size'];
-    $file_tmp =$_FILES['image']['tmp_name'];
-    $file_type=$_FILES['image']['type'];
-    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-
-    $expensions= array("jpeg","jpg","png");
-
-    if(in_array($file_ext,$expensions)=== false){
-        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-    }
-
-    if($file_size > 2097152){
-        $errors[]='File size must be excately 2 MB';
-    }
-
-    if(empty($errors)==true){
-        move_uploaded_file($file_tmp,"images/".$file_name);
-        echo "Success";
-    }else{
-        print_r($errors);
-    }
-}
-?>
-
-<form action = "" method = "POST" enctype = "multipart/form-data">
-    <input type = "file" name = "image" />
-    <input type = "submit"/>
-
-    <ul>
-        <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
-        <li>File size: <?php echo $_FILES['image']['size'];  ?>
-        <li>File type: <?php echo $_FILES['image']['type'] ?>
-    </ul>
-
-</form>
-
-<!--
 <form action="upload.php" method="post" enctype="multipart/form-data">
-    Profiilipildi ülslaadimine
+    Vali fail:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Lae üles" name="submit">
 </form>
--->
+
 </body>
