243055092844fac2a0681389c46c6af89f6e1748
Helen Õunapuu <helen.ounapuu@gmail.com>
2018-03-31 21:52:49 +0300

    Pangalink vrs5

--- html/contact.php
+++ html/contact.php
@@ -1,6 +1,8 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
+<?php
+include_once "pay.php";
+?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
@@ -20,6 +22,15 @@
             });
         });
     </script>
+    <p>Palju muud kontaktinfot <br/></p>
+
+    <form method="post" action="http://localhost:3480/banklink/swedbank">
+        <!-- include all values as hidden form fields -->
+        <?php foreach($fields as $key => $val):?>
+            <input type="hidden" name="<?php echo $key; ?>" value="<?php echo htmlspecialchars($val); ?>" />
+        <?php endforeach; ?>
+            <tr><td colspan="2"><input type="submit" value="Edasi panga lehele" /></td></tr>
+    </form>
 
     get:
     <?php
--- html/pay.php
+++ html/pay.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Example payment usage - Swedbank - pangalink.net</title>
-    </head>
-    <body>
 <?php
 
 // THIS IS AUTO GENERATED SCRIPT
@@ -101,29 +94,3 @@ $fields["VK_MAC"] = base64_encode($signature);
 // STEP 5. Generate POST form with payment data that will be sent to the bank
 // ==========================================================================
 ?>
-
-        <h1><a href="http://localhost:3480/">Pangalink.net</a></h1>
-        <p>Makse teostamise näidisrakendus <strong>"Swedbank"</strong></p>
-
-        <form method="post" action="http://localhost:3480/banklink/swedbank">
-            <!-- include all values as hidden form fields -->
-<?php foreach($fields as $key => $val):?>
-            <input type="hidden" name="<?php echo $key; ?>" value="<?php echo htmlspecialchars($val); ?>" />
-<?php endforeach; ?>
-
-            <!-- draw table output for demo -->
-            <table>
-<?php foreach($fields as $key => $val):?>
-                <tr>
-                    <td><strong><code><?php echo $key; ?></code></strong></td>
-                    <td><code><?php echo htmlspecialchars($val); ?></code></td>
-                </tr>
-<?php endforeach; ?>
-
-                <!-- when the user clicks "Edasi panga lehele" form data is sent to the bank -->
-                <tr><td colspan="2"><input type="submit" value="Edasi panga lehele" /></td></tr>
-            </table>
-        </form>
-
-    </body>
-</html>
