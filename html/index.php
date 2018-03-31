1ef80be81df96a2feeef80d54d65e0917d9edafa
Raul Lehesalu <raul.lehesalu@gmail.com>
2018-03-31 20:24:43 +0300

    try using CDN jquery resource from ajax.googleapis.com. Fallback to local when CDN not available.

--- html/index.php
+++ html/index.php
@@ -2,16 +2,16 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
-<?php include_once("i18n/i18n.php");
-require_once __DIR__ . '/php-graph-sdk-5.4/src/Facebook/autoload.php';
-require_once "database-handler.php";
+<?php
+    include_once("i18n/i18n.php");
+    require_once __DIR__ . '/php-graph-sdk-5.4/src/Facebook/autoload.php';
+    require_once "database-handler.php";
 
-if (!session_id()) { //Check if facebook session is up, if not then start a new one
-    session_start();
-}
+    if (!session_id()) { //Check if facebook session is up, if not then start a new one
+        session_start();
+    }
 
-use Facebook\FacebookRequestException;
-
+    use Facebook\FacebookRequestException;
 ?>
 
 
@@ -23,7 +23,13 @@ use Facebook\FacebookRequestException;
 
     <link rel="stylesheet" type="text/css" href="styles/style.css"/>
     <script src="js/googlemaps.js" type="text/javascript"></script>
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <!--<script src="https://code.jquery.com/jquery-1.10.2.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        if (typeof jQuery == 'undefined') {
+            document.write(unescape("%3Cscript src='/js/jquery-3.3.1.min.js' type='text/javascript'%3E%3C/script%3E"));
+        }
+    </script>
 </head>
 
 <body onload="initMap()">
