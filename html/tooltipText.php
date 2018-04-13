ed5d2aa35098b4abcb9f633346d20b36f3591e14
Helen Õunapuu <helen.ounapuu@gmail.com>
2018-04-13 22:06:53 +0300

    AJAX tooltip v3 test

--- html/tooltipText.php
+++ html/tooltipText.php
@@ -1,6 +1,6 @@
 <?php
     $request = simplexml_load_string($GET["request"]);
-
+    print_r($request);
     if ($request == "tooltip") {
         echo "<tooltip>Vabandame! Me endiselt töötame oma lehe parendamise suunas. Rohkema informatsiooni vaatamiseks proovide uuesti järgmisel etapil.</tooltip>";
     }
