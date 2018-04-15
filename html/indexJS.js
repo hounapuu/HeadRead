79f5a0ee76507ed0f517a18c7d4a04bf9566a1cc
Helen Õunapuu <helen.ounapuu@gmail.com>
2018-04-15 19:17:48 +0300

    js file

---
+++ html/indexJS.js
@@ -0,0 +1,23 @@
+$(function () {
+    $("#navbar-placeholder").load("navigationbar.html", function () {
+        $("#navbarHome").addClass("navbarActive");
+    });
+});
+var firstLoad = true;
+
+function mouseOver() {
+    if (firstLoad) {
+        firstLoad = false;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                xmlDoc = $.parseXML(this.responseText);
+                $xml = $(xmlDoc);
+                document.getElementById("tooltip").innerHTML =
+                    $xml.find("tooltip").text();
+            }
+        };
+        xhttp.open("GET", "tooltipText.php?request=<request>tooltip</request>", true);
+        xhttp.send();
+    }
+}
